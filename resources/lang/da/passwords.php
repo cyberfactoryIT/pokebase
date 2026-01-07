3ea49d69830fabd1055630ed13f937ffd563029f
cyberfactoryIT <32998492+cyberfactoryIT@users.noreply.github.com>
2026-01-07 10:42:25 +0100

    Create passwords.php

---
+++ resources/lang/da/passwords.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines - Dansk
+    |--------------------------------------------------------------------------
+    */
+
+    'reset' => 'Din adgangskode er blevet nulstillet.',
+    'sent' => 'Vi har sendt dig et link til nulstilling af adgangskode via e-mail.',
+    'throttled' => 'Vent venligst før du prøver igen.',
+    'token' => 'Dette token til nulstilling af adgangskode er ugyldigt.',
+    'user' => "Vi kan ikke finde en bruger med den e-mailadresse.",
+
+];
