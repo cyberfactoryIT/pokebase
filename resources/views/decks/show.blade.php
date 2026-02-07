9962d2d13491c002b7902e4226fb81a6a1557a1a
cyberfactoryIT <32998492+cyberfactoryIT@users.noreply.github.com>
2026-02-07 13:15:03 +0100

    Update show.blade.php

--- resources/views/decks/show.blade.php
+++ resources/views/decks/show.blade.php
@@ -309,10 +309,7 @@
                         <a href="{{ route('billing.index') }}" class="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-lg font-medium transition">
                             {{ __('prices.hidden.cta_upgrade') }}
                         </a>
-                        <span class="text-gray-400 flex items-center">{{ __('prices.hidden.or') }}</span>
-                        <a href="{{ route('deck-evaluation.packages.index') }}" class="px-6 py-3 bg-purple-600 hover:bg-purple-700 text-white rounded-lg font-medium transition">
-                            {{ __('prices.hidden.cta_deck_evaluation') }}
-                        </a>
+                        
                     </div>
                 </div>
             </div>
